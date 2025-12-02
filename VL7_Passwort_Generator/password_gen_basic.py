58fe2c42b63c5f2504c30226e58f2a5a097227db
Lukas Lodes <lukas.lodes@gmail.com>
2025-12-02 11:39:29 +0100

    PW Gen solution

--- VL7_Passwort_Generator/password_gen_basic.py
+++ VL7_Passwort_Generator/password_gen_basic.py
@@ -1,25 +1,43 @@
 import random
 import tkinter as tk
-from string import punctuation
+import string
 
 # Paths maybe absolute or relative - depends on how you execute the script
 adjectives_path = "/Users/lodes/Work/git/swd_wi_ws25/pw_gen_sol/adjektive.txt"
 nouns_path = "/Users/lodes/Work/git/swd_wi_ws25/pw_gen_sol/nomen.txt"
 
 
-def read_file(file):
-    pass
+def read_file(file_path):
+    with open(file_path) as f:
+        lines = f.readlines()
+        cleaned_lines = [line.rstrip() for line in lines]
+    
+    return cleaned_lines
 
 def generate_password():
-    pass
+    adjectives = read_file(adjectives_path)
+    nouns = read_file(nouns_path)
+
+    adjective = random.choice(adjectives)
+    noun = random.choice(nouns)
+    number = random.randint(0, 100)
+    sc = random.choice(string.punctuation)
+
+    password = f"{adjective}{noun}{number}{sc}"
+    password_label.config(text=password) 
+    return password
+
 
 # Create Tkinter window
 root = tk.Tk()
-root.title("TODO")
+root.title("Password Generator")
 
 # Create and place the Generate Password button
-generate_button = tk.Button(root, text="")
+generate_button = tk.Button(root, text="Generate Password", command=generate_password)
+generate_button.pack()
 
 # Create a label to display the generated password
-password_label = tk.Label(root, text="")
+password_label = tk.Label(root, text="Generated PW: ")
+password_label.pack()
 
+root.mainloop()
