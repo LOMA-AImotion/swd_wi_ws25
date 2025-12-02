944c0bfec5b47f080fcbe5364a2ddab8987642a6
Lukas Lodes <lukas.lodes@gmail.com>
2025-12-02 10:17:17 +0100

    Upload barebone for PW Gen

---
+++ VL7_Passwort_Generator/password_gen_basic.py
@@ -0,0 +1,25 @@
+import random
+import tkinter as tk
+from string import punctuation
+
+# Paths maybe absolute or relative - depends on how you execute the script
+adjectives_path = "/Users/lodes/Work/git/swd_wi_ws25/pw_gen_sol/adjektive.txt"
+nouns_path = "/Users/lodes/Work/git/swd_wi_ws25/pw_gen_sol/nomen.txt"
+
+
+def read_file(file):
+    pass
+
+def generate_password():
+    pass
+
+# Create Tkinter window
+root = tk.Tk()
+root.title("TODO")
+
+# Create and place the Generate Password button
+generate_button = tk.Button(root, text="")
+
+# Create a label to display the generated password
+password_label = tk.Label(root, text="")
+
